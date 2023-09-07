65302967041c146305b281e128d1dd6e1f53b743
Ricardo Bohadana Martins <ricardo.bohadana@fu2re.com.br>
2023-09-07 20:28:48 -0300

    create table and add data

--- src/index.js
+++ src/index.js
@@ -9,6 +9,23 @@ const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
+const connection = mysql.createConnection(config);
+connection.query(
+  "CREATE TABLE IF NOT EXISTS Names (id int auto_increment, name VARCHAR(255), primary key (id))",
+  (err, result) => {
+    if (err) throw err;
+    console.log("Table created");
+  }
+);
+connection.query(
+  "INSERT INTO Names (name) VALUES ('Gustavo'), ('Santos'), ('Ricardo')",
+  (err, result) => {
+    if (err) throw err;
+    console.log("Data inserted");
+  }
+);
+connection.end();
+
 app.get("/", (req, res) => {
   const connection = mysql.createConnection(config);
   connection.query("SELECT * FROM Names", (err, result) => {
